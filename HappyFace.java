5035d9013c0f0654c590bd02cd55c5ab416c889f
HckABz <91646513+HckABz@users.noreply.github.com>
2022-12-21 16:56:39 -0800

    Add files via upload

---
+++ HappyFace.java
@@ -0,0 +1,16 @@
+/*
+ * Alex Bravo
+ * CS A170
+ * July 1,2022
+ * 
+ * Excercise/Exam 1
+ */
+
+public class HappyFace
+{
+    public static void main(String[] args)
+    {
+        System.out.println("  /////\n +\"\"\"\"\"+ \n(| o o |)\n |  ^  |");
+        System.out.println(" | \'-\' |\n +-----+");
+    }
+}
---
+++ MathPrinter.java
@@ -0,0 +1,16 @@
+/*
+ * Alex Bravo
+ * CS A170
+ * June 30, 2022
+ * 
+ * Exercise/Exam 1
+ */
+
+public class MathPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1+3+5+7+9);
+        System.out.println(1*3*5*7*9);
+    }
+}
